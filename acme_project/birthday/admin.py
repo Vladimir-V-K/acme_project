411c00532078edfb7982ca4ca4e1ce8f3e428114
Владимир Крюков <vladimir-krukov1984@yandex.ru>
2023-09-07 08:31:42 +0300

    без картинки

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,21 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+admin.site.empty_value_display = 'Планета Земля'
+
+
+class BirthdayInLine(admin.StackedInline):
+    model = Birthday
+    extra = 0
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday'
+    )
+
+
+admin.site.register(Birthday, BirthdayAdmin)
